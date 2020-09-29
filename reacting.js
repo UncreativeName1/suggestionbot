0a30733fb7f80ed677373ef5beef8c1100df39b0
lordligma12 <70034924+lordligma12@users.noreply.github.com>
2020-09-29 00:44:50 -0400

    Update reacting.js

--- reacting.js
+++ reacting.js
@@ -30,4 +30,17 @@ client.on('message', message => {
     }
 });
 
+client.on("messageReactionAdd", async (reaction, user) => {
+    let message = reaction.message;
+    if (user.bot) return;
+    else if (message.member.roles.cache.has('422713631973834754' || '676077597376905244' || '410252008965144596'))
+        if (reaction.emoji === '🗑️') {
+            message.delete();
+            message.author.send('Hey' + '<@' + message.author.id + '>, ' + 'unfortunately, your suggestion was rejected. We may reconsider this in the future but have no plans to implement this. \n Your ' + reaction.message);
+        }else if (reaction.emoji === '⭐') {
+            message.pin();
+            message.author.send('Hey' + '<@' + message.author.id + '>, ' + 'we\'re happy to announce that we\'ve accepted your suggestion! It will be implemented into the server very soon, so stay tuned! \n Your ' + reaction.message)
+        }else return;
+    else return;
+
 client.login('NzU5OTk4NjQ2NDY4NzM5MTEy.X3Fp5Q.txqfKnHZmke7kxC12K2ybpAj0KA');
